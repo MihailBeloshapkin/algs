4840e197d640c644ae29659168ff09ed1ff9bfaf
MihailBeloshapkin <mihail.beloshapkin.mm@yandex.ru>
2023-01-03 16:11:11 +0300

    add

--- parsers/parse.ml
+++ parsers/parse.ml
@@ -13,37 +13,51 @@ let is_digit = function '0'..'9' -> true | _ -> false
 
 let integer = take_while1 is_digit
 
-let sign =
-  peek_char
-  >>= function
-    | Some '-' -> advance 1 >>| fun () -> "-"
-    | Some '+' -> advance 1 >>| fun () -> "+"
-    | Some c when (is_digit c) -> return "+"
-    | _ -> fail "Sign or digit expected"
-;;
 
-let dot =
-  peek_char
-  >>= function 
-    | Some '.' -> advance 1 >>| fun () -> true
-    | _ -> return false
-;;
+module NumParser = struct
+  let sign =
+    peek_char
+    >>= function
+      | Some '-' -> advance 1 >>| fun () -> "-"
+      | Some '+' -> advance 1 >>| fun () -> "+"
+      | Some c when (is_digit c) -> return "+"
+      | _ -> fail "Sign or digit expected"
+  ;;
 
-let number =
-  sign
-  >>= fun sign ->
-  take_while1 is_digit
-  >>= fun whole ->
-  dot
-  >>= function
-  | false -> 
-    return (float_of_string (sign ^ whole))
-  | true -> take_while1 is_digit >>= fun part ->
-    return (float_of_string (sign ^ whole ^ "." ^ part))
+  let dot =
+    peek_char
+    >>= function 
+      | Some '.' -> advance 1 >>| fun () -> true
+      | _ -> return false
+  ;;
+
+  let number =
+    sign
+    >>= fun sign ->
+    take_while1 is_digit
+    >>= fun whole ->
+    dot
+    >>= function
+    | false -> 
+      return (float_of_string (sign ^ whole))
+    | true -> take_while1 is_digit >>= fun part ->
+      return (float_of_string (sign ^ whole ^ "." ^ part))
+
+end
+
+module KV = struct
+  let key = take_while1 (function 'a'..'z' -> true | _ -> false)
+
+  let value = take_while1 (function 'a'..'z' -> true | '0'..'9' -> true | _ -> false)
+
+  let kv =
+    key <* whitespace
+    >>= fun k -> value >>= fun v -> return (k, v)
+end
 
 let () =
-  let result = Angstrom.parse_string (whitespace *> number <* whitespace) ~consume:Angstrom.Consume.All " -1234.56789 " in 
+  let result = Angstrom.parse_string (whitespace *> KV.kv <* whitespace) ~consume:Angstrom.Consume.All " token afg646yhdy6 " in 
   match result with
-  | Result.Ok x -> printf "%f\n" x
+  | Result.Ok (k, v) -> printf "%s %s \n" k v
   | _ -> printf "SOMETHING WENT WRONG\n"
 ;;
